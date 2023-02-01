187ab6e44ff852857ea4ff99129ead67306e9fc4
Gurdev Singh <106698199+whogurdevil@users.noreply.github.com>
2023-02-01 23:19:59 +0530

    added feb 1 problems and react native

---
+++ leetcode/removeDuplicates.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        set<int> s;
+
+        for(int i = 0; i<nums.size(); i++){
+            s.insert(nums[i]);
+        }
+
+        nums.clear();
+
+        for(int i : s){
+            nums.push_back(i);
+        }
+
+        return s.size();
+
+    }
+
+};
