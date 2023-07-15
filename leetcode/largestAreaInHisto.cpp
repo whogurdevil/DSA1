17383fbb8b862149a58287b256d22ba8ea684959
Gurdev Singh <106698199+whogurdevil@users.noreply.github.com>
2023-07-15 17:21:17 +0530

    added problems for july15

---
+++ leetcode/largestAreaInHisto.cpp
@@ -0,0 +1,18 @@
+#include<stack>
+vector<int> nextSmallerElement(vector<int> &arr, int n)
+{
+    vector<int> ans(n);
+    stack<int> s;
+
+    s.push(-1);
+
+    for(int i = n-1; i >= 0; i--) {
+        while(s.top() >= arr[i]) {
+            s.pop();
+        }
+        ans[i] = s.top();
+        s.push(arr[i]);
+    }
+
+    return ans;
+}
