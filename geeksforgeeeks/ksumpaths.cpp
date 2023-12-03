6eda26042311559d5b88e53de7839e47f22e10e9
Gurdev Singh <106698199+whogurdevil@users.noreply.github.com>
2023-12-03 15:46:20 +0530

    added prblem for des 3

---
+++ geeksforgeeeks/ksumpaths.cpp
@@ -0,0 +1,161 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node 
+{
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+// Function to Build Tree
+Node *buildTree(string str) 
+{
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;)
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node *root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node *> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node *currNode = queue.front();
+        queue.pop();
+
+        // Get the current Node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current Node
+            currNode->left = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current Node
+            currNode->right = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+// } Driver Code Ends
+//User function template for C++
+
+/*
+struct Node 
+{
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+class Solution{
+  public:
+    void solve(Node* root, int &count, int k, vector<int> nodes){
+        if(root == NULL){
+            
+            return;
+        }
+        
+        nodes.push_back(root->data);
+        solve(root->left, count, k, nodes);
+        solve(root->right, count, k, nodes);
+        
+        int sum = 0;
+        for(int i = nodes.size() - 1; i >= 0; i--){
+            sum += nodes[i];
+            
+            if(sum == k){
+                count++;
+            }
+            // if(sum > k)
+            //     break;
+        }
+        nodes.pop_back();
+    }
+    
+    
+    int sumK(Node *root,int k)
+    {
+        if(!root)
+            return 0;
+            
+        int count = 0;
+        vector<int> nodes;
+        solve(root, count, k, nodes);
+        
+        return count;
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    string tc;
+    getline(cin, tc);
+    int t = stoi(tc);
+  
+    while(t--)
+    {
+        string s ,ch;
+        getline(cin, s);
+        Node* root = buildTree(s);
+
+        string key;
+        getline(cin, key);
+        int k=stoi(key);
+        Solution ob;
+        cout << ob.sumK(root, k) << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
